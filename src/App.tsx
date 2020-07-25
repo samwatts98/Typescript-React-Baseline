bfdbff2969392d3c304a5b13b62bab9d9e174fa1
Sam Watts <samjwatts98@gmail.com>
2020-07-25 19:18:39 +0100

    introduced styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Title } from './components/Title';
 
 function App(): React.ReactElement {
-  return <h1>Hello World!</h1>;
+  return <Title>Hello World!</Title>;
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+export const Title = styled.h1`
+  text-align: center;
+  transition: color 0.5s;
+  &:hover {
+    color: purple;
+    font-size: 2rem;
+  }
+`;
